22c5a4ff63fe25e9c48d581cfc1c89579bbd0861
Daniel Alp <daniel.alp.email@gmail.com>
2025-02-14 01:03:53 -0500

    simplified parsing

--- scheme_interpreter/parse.py
+++ scheme_interpreter/parse.py
@@ -1,35 +1,23 @@
 from collections import deque
 
-def represent_num(token: str):
-    try:
-        float(token)
-    except ValueError:
-        return False
-    return True
-
 type expr = list[expr] | float | str
 
+# TODO error handling
 def parse(tokens: deque[str]) -> expr:
     next = tokens.popleft()
     if next == '(':
         expr = []
-        expr.append(parse(tokens))
         while tokens[0] != ')':
             expr.append(parse(tokens))
         tokens.popleft()
         return expr
-    elif represent_num(next):
-        return float(next)
     else:
-        return next
+        return atom(next)
+
+def atom(token):
+    try:
+        return float(token)
+    except ValueError:
+        return token
 
 print(parse(deque(['(', '+', '34', '(', '*', '4', '5', ')', '7', ')'])))
-print(parse(deque(['1'])))
-try:
-    print(parse(deque(['(', '+', '1'])))
-except Exception:
-    print("Error")
-try:
-    print(parse(deque(['(', '+', '1', '(', '*', '+', ')'])))
-except Exception:
-    print("Error")
